3c1c01de80fd9dbfc5710f3c208eddbe3f67a1bc
Sultana Razia <sultanarazia483@gmail.com>
2022-08-23 09:50:41 +0600

    Google Sign using custom hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './Login.css';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h2>Please Login</h2>
             <div style={{ margin: '20px' }}>
-                <button>Google sign in</button>
+                <button onClick={signInWithGoogle}>Google sign in</button>
             </div>
             <form>
                 <input type="email" placeholder="Your Email" />
